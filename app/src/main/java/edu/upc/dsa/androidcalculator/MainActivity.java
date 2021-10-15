be81a92ddc88684e28dc13416c1bb4b38011e169
albamzgil <amgalba5@gmail.com>
2021-10-15 11:58:06 +0200

    Interfaz calculadora grafica

--- app/src/main/java/edu/upc/dsa/androidcalculator/MainActivity.java
+++ app/src/main/java/edu/upc/dsa/androidcalculator/MainActivity.java
@@ -2,13 +2,26 @@ package edu.upc.dsa.androidcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView;
+        textView = findViewById(R.id.Pantalla);
     }
+
+    /*public void onClick(View v){
+        Button bt = (Button)v;
+        textView.setText(bt.getText());
+    }*/
 }
