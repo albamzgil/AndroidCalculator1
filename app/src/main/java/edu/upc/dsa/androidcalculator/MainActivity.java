0349208193707908576107368a7b55329fad7b37
albamzgil <amgalba5@gmail.com>
2021-10-22 11:36:18 +0200

    Android Calculadora

--- app/src/main/java/edu/upc/dsa/androidcalculator/MainActivity.java
+++ app/src/main/java/edu/upc/dsa/androidcalculator/MainActivity.java
@@ -18,28 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected TextView textView;
     private ArrayList<CharSequence> numsescritos = new ArrayList<CharSequence>();
     private ArrayList<Integer> operacion = new ArrayList<Integer>();
+    int n = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.resultado);
+        textView.setText("");
     }
 
     public void onClick(View v){
+        if (n == 1){
+            textView.setText("");
+            n = 0;
+        }
         Button bt = (Button)v;
-        textView.setText(bt.getText());
-        CharSequence num = bt.getText();
-        numsescritos.add(num);
+        textView.setText(textView.getText().toString()+bt.getText());
     }
 
     public void operation(View v){
+        CharSequence num = textView.getText();
+        numsescritos.add(num);
         Button btoperation = (Button)v;
-        textView.setText(btoperation.getText());
+        textView.setText("");
         operacion.add(btoperation.getId());
     }
 
     public void Equal(View v){
+        CharSequence num = textView.getText();
+        numsescritos.add(num);
+        n = 1;
         if (operacion.get(0)==R.id.suma){
             CharSequence num1c = numsescritos.get(0);
             CharSequence num2c = numsescritos.get(1);
@@ -63,17 +72,17 @@ public class MainActivity extends AppCompatActivity {
             CharSequence num2c = numsescritos.get(1);
             int num1 = Integer.parseInt((String) num1c);
             int num2 = Integer.parseInt((String) num2c);
-            int resultint = num1 * num2;
+            int resultint = num1*num2;
             String result = Integer. toString(resultint);
             textView.setText(result);
         }
         if (operacion.get(0)==R.id.div){
             CharSequence num1c = numsescritos.get(0);
             CharSequence num2c = numsescritos.get(1);
-            int num1 = Integer.parseInt((String) num1c);
-            int num2 = Integer.parseInt((String) num2c);
-            int resultint = num1 / num2;
-            String result = Integer. toString(resultint);
+            double num1 = Double.parseDouble((String) num1c);
+            double num2 = Double.parseDouble((String) num2c);
+            double resultdoub = num1 / num2;
+            String result = Double. toString(resultdoub);
             textView.setText(result);
         }
         if (operacion.get(0)==R.id.sin){
